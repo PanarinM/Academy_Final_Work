da0f43581d674355d956c4ba6c9f88050a6d6299
PanarinM <michaelpanarin@inbox.ru>
2017-04-27 13:33:05 +0300

    getting ready for heroku v5

--- worst_buy/settings.py
+++ worst_buy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n3wmm3napdpq4kc!jzu$xx(=qqlqd7*a9t+q)1&5k-%z!+$oy0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['worst-buy.herokuapp.com']
 
