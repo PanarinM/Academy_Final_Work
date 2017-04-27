21a12c0fc0fce9d3140e9cf61d0acd59aa17851f
PanarinM <michaelpanarin@inbox.ru>
2017-04-27 13:39:41 +0300

    getting ready for heroku v6

--- worst_buy/settings.py
+++ worst_buy/settings.py
@@ -96,6 +96,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
